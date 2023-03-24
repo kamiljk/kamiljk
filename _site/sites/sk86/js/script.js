ecfa464c1d32dfedf595252c49885af544bba17c
kamiljk <kamil.j.krawczyk@gmail.com>
2023-03-24 14:41:58 -0500

    clorging

---
+++ _site/sites/sk86/js/script.js
@@ -0,0 +1,12 @@
+const images = [
+    "/sk86_bg_1.png",
+    "/sk86_bg_2.png",
+    "/sk86_bg_3.png",
+    "/sk86_bg_4.png"
+];
+
+const randomIndex = Math.floor(Math.random() * images.length);
+
+const randomImage = images[randomIndex];
+
+document.getElementById("random-image").setAttribute("src", "img/bg/" + randomImage);
